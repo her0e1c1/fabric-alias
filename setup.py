18da7e4d2fadc70436cac2757e38d8998f0e34fc
test <none@none>
2015-05-03 14:02:29 +0000

    add setup.py and README.rst

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+install_requires = ['fabric']
+
+setup(
+    name='Fabric-Alias',
+    author='Hiroyuki Ishii',
+    packages=find_packages(),
+    install_requires=install_requires,
+)
