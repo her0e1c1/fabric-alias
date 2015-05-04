bce3e4cb9876057c5690d72c2a3b0c1935f441d1
test <none@none>
2015-05-04 07:18:54 +0000

    モジュールのdefaultのtaskを実行できるようにした

--- fabric_alias/__init__.py
+++ fabric_alias/__init__.py
@@ -2,17 +2,21 @@
 
 def set_alias(modules={}, seen_clear=False):
     from fabric.main import load_tasks_from_module, is_task_module, _seen
+    from fabric.task_utils import _Dict
     from fabric import state
-    import os
-    dirname, _ = os.path.split(__file__)
 
-    c = {}
-    for import_name, module in modules.items():
+    new_style_tasks = _Dict()
+    for name, module in modules.items():
         if is_task_module(module):
-            docs, newstyle, classic, defaul = load_tasks_from_module(module)
-            c[import_name] = newstyle
+            docs, newstyle, classic, default = load_tasks_from_module(module)
+            for task_name, task in newstyle.items():
+                if name not in new_style_tasks:
+                    new_style_tasks[name] = _Dict()
+                new_style_tasks[name][task_name] = task
+            if default is not None:
+                new_style_tasks[name].default = default
 
-    state.commands.update(c)
+    state.commands.update(new_style_tasks)
 
     if seen_clear:
         _seen.clear()
