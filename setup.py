5c8ccd0941b0316ccb869916ca788b92dd168317
test <none@none>
2015-05-04 07:26:57 +0000

    version 0.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 install_requires = ['fabric']
-version = '0.0.1'
+version = '0.0.2'
 name = 'fabric_alias'
 short_description = 'fabric_alias is a plugin for fabric when using fabfile.'
 long_description = """\
